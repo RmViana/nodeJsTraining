40b7d1bdeb33005627856bf4cceaca99c7d58b4e
Romildo Alves <raj.alves96@gmail.com>
2018-06-24 16:52:28 -0300

    Testing lodash's uniq method

--- notes-node/app.js
+++ notes-node/app.js
@@ -5,8 +5,11 @@ const os    = require('os');
 const _     = require('lodash');
 const notes = require('./notes.js');
 
-console.log(_.isString(true));
-console.log(_.isString('Wade'));
+// console.log(_.isString(true));
+// console.log(_.isString('Wade'));
+
+var filterArray = _.uniq(['Romildo', 1, 'Romildo', 1, 2, 3, 4]);
+console.log(filterArray);
 
 // console.log('Result : ', notes.add(9, -2));
 
