4806199865b5f720476c4108a7b4b90348a8cdc1
Romildo Alves <raj.alves96@gmail.com>
2018-06-24 16:20:30 -0300

    Exporting a note's function (addNote) and displaying  the result on the app file

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,5 +4,8 @@ const fs    = require('fs');
 const os    = require('os');
 const notes = require('./notes.js');
 
-var user = os.userInfo();
-fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+var res = notes.addNote();
+console.log(res);
+
+// var user = os.userInfo();
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
