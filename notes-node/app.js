7978226ebaea3ee5a5cc96b62921fb8ec4c95ac5
Romildo Alves <raj.alves96@gmail.com>
2018-07-01 17:46:25 -0300

    Updating the verification

--- notes-node/app.js
+++ notes-node/app.js
@@ -8,14 +8,14 @@ const notes = require('./notes.js');
 var command = process.argv[2];
 console.log('Command: ', command);
 
-if(command == 'add'){
+if(command === 'add'){
   console.log('Adding new  note');
-}else if(command == 'list'){
+}else if(command === 'list'){
   console.log('Listing all notes');
-}else if(command == 'read'){
-    console.log('Fetching notes');
-}else if(command == 'remove'){
-  console.log('Removing notes');
+}else if(command === 'read'){
+    console.log('Reading note');
+}else if(command === 'remove'){
+  console.log('Removing note');
 }else{
   console.log('Command not recognized');
 }
