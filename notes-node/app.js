4957596244937450bc15e7fab1411b78f78323fe
Romildo Alves <raj.alves96@gmail.com>
2018-10-02 20:17:27 -0300

    Listing all the notes

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,15 +1,11 @@
-console.log('Starting app.js');
-
 const fs    = require('fs');
-const _     = require('lodash');
+// const _     = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
 var command = argv._[0];
-console.log('Command: ', command);
-console.log('Yargs', argv);
 
 if(command === 'add'){
   var note = notes.addNote(argv.title, argv.body);
@@ -17,7 +13,9 @@ if(command === 'add'){
   console.log(message);
   note ? notes.logNote(note) : '';
 }else if(command === 'list'){
-  notes.getAll();
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => notes.logNote(note));
 }else if(command === 'read'){
   var note = notes.getNote(argv.title);
   var message = note ? 'Note read!' : 'Note not found.';
