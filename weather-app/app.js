6b713e4483cc5c470b75d3044c95205fdf6e5e1f
Romildo Alves <raj.alves96@gmail.com>
2018-11-15 12:04:56 -0200

    Fetching the weather based on the latitude/ longitude

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,38 @@
-const yargs   = require('yargs');
-const geocode = require('./geocode/geocode');
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Address to fetch weather for',
-      string: true
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+// const yargs   = require('yargs');
+// const geocode = require('./geocode/geocode');
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Address to fetch weather for',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+//   geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if (errorMessage){
+//       console.log(errorMessage);
+//     }else {
+//       console.log(JSON.stringify(results, undefined, 2));
+//     }
+//   });
 
-  geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if (errorMessage){
-      console.log(errorMessage);
-    }else {
-      console.log(JSON.stringify(results, undefined, 2));
-    }
-  });
+//137746b7b19794ec20897249e41d05ae
+
+const request = require('request');
+
+
+request({
+  url : 'https://api.darksky.net/forecast/137746b7b19794ec20897249e41d05ae/-23.5524278,-46.5515876',
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+      console.log(body.currently.temperature);
+  } else {
+      console.log('Unable to fetch weather.');
+  }
+});
