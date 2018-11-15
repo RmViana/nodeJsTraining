0c32b9078009740940e3c2b4cbb4cf5b77a5aec0
Romildo Alves <raj.alves96@gmail.com>
2018-11-15 10:18:07 -0200

    Pretty printer to the body of the response

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyB5lD9sP7GAKVHkDhgZBl_W2KCT3zIVWFA',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
