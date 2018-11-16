325a5772840572de25e6f8990452e407a25894e0
Romildo Alves <raj.alves96@gmail.com>
2018-11-16 17:58:57 -0200

    Projects page title

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -49,7 +49,7 @@ app.get('/about', (req, res) => {
 
 app.get('/projects', (req, res) => {
   res.render('projects.hbs', {
-    pageText   : 'Portfolio page here!',
+    pageTitle   : 'Projects',
   });
 });
 
