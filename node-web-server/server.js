b849b2cd4fa9768b81e2b558a6949c16418b8939
Romildo Alves <raj.alves96@gmail.com>
2018-11-16 09:34:54 -0200

    Creating middlewares

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const hbs     = require('./node_modules/hbs');
+const fs      = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append to server log.');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -16,14 +34,6 @@ hbs.registerHelper('screamIt', (text) => {
 });
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
-  // res.send({
-  //   name: 'Wade',
-  //   likes: [
-  //     "chess",
-  //     "technology"
-  //   ]
-  // });
   res.render('home.hbs', {
     pageTitle : 'Home Page',
     welcomeMessage : 'Welcome to my Website!'
@@ -43,6 +53,5 @@ app.get('/bad', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log(__dirname);
   console.log('Server is up on port 3000');
 });
