2af363267ea4bc85d47d5b244457adbf4a4cfe5a
Romildo Alves <raj.alves96@gmail.com>
2018-11-16 09:07:28 -0200

    Handlebars for reusable templates

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const hbs     = require('hbs');
+const hbs     = require('./node_modules/hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
@@ -17,15 +26,13 @@ app.get('/', (req, res) => {
   // });
   res.render('home.hbs', {
     pageTitle : 'Home Page',
-    currentYear : new Date().getFullYear(),
-    welcomeMessage : 'Welcome to the Home Page!'
+    welcomeMessage : 'Welcome to my Website!'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle   : 'About Page',
-    currentYear : new Date().getFullYear()
   });
 });
 
