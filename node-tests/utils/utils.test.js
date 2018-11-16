6bd496d6e335fdc99af92afe1527a43924f7710a
Romildo Alves <raj.alves96@gmail.com>
2018-11-16 19:47:28 -0200

    Working with assertion library (expect)

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,40 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
-  var res = utils.square(2);
-  if (res !== 4) {
-    throw new Error(`Expected 4, but got ${res}`);
-  }
-})
+  var res = utils.square(3);
+
+  expect(res).toBe(9).toBeA('number');
+});
+
+it('should set firstName and lastName', () => {
+  var userObject = {
+    age: 22,
+    location: 'São Paulo'
+  };
+  var res = utils.setName(userObject, "Wade Wilson");
+  expect(res).toInclude({
+    firstName: "Wade",
+    lastName : "Wilson"
+  }).toBeA('object');
+});
+
+// it('should expect some values', () => {
+//   // expect(12).toNotBe(11);
+//   // expect({name: 'Wade'}).toEqual({name: 'Wade'});
+//   // expect([2,3,4]).toExclude(1);
+//   expect({
+//     name: 'Wade',
+//     age: 22,
+//     location: 'São Paulo'
+//   }).toExclude({
+//     age: 23
+//   });
+// });
