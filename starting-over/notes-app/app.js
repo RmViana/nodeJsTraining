3e38475dcfc784404ef49bce0bd1e573471bdc33
Romildo Alves <raj.alves96@gmail.com>
2019-06-03 20:36:58 -0300

    Getting back to work

--- starting-over/notes-app/app.js
+++ starting-over/notes-app/app.js
@@ -1,7 +1,5 @@
 const fs = require('fs')
 
-// fs.writeFileSync('notes.txt', 'My name is Wade.');
+fs.writeFileSync('notes.txt', 'My name is Wade.');
 
-fs.appendFileSync('notes.txt', '\nAnd my surname is Wilsom!')
-
-//appendFileSync to append to the file
+fs.appendFileSync('notes.txt', '\nAnd my surname is Wilson!');
