6d67323cb803a11c29ebba36d2f768333a7f0ed6
Romildo Alves <raj.alves96@gmail.com>
2019-06-04 20:45:45 -0300

    Working with local modules

--- starting-over/notes-app/app.js
+++ starting-over/notes-app/app.js
@@ -1,5 +1,20 @@
-const fs = require('fs')
+const getNotes = require('./notes')
 
-fs.writeFileSync('notes.txt', 'My name is Wade.');
+const msg = getNotes()
 
-fs.appendFileSync('notes.txt', '\nAnd my surname is Wilson!');
+console.log(msg)
+
+
+
+
+// const fs = require('fs')
+
+// fs.writeFileSync('notes.txt', 'My name is Wade.');
+
+// fs.appendFileSync('notes.txt', '\nAnd my surname is Wilson!');
+
+// const add = require('./utils.js')
+
+// const sum = add(4, -2)
+
+// console.log(sum)
