99cfd375a99ca425851e3f4f03c674818a7b4592
Romildo Alves <raj.alves96@gmail.com>
2019-06-10 20:31:52 -0300

    Nodemon and command line arguments

--- starting-over/notes-app/app.js
+++ starting-over/notes-app/app.js
@@ -1,20 +1,10 @@
 const chalk    = require('chalk')
 const getNotes = require('./notes')
 
-const msg = getNotes()
+const command = process.argv[2];
 
-console.log(msg)
-console.log(chalk.green.bold.inverse("Success!"))
-
-
-// const fs = require('fs')
-
-// fs.writeFileSync('notes.txt', 'My name is Wade.');
-
-// fs.appendFileSync('notes.txt', '\nAnd my surname is Wilson!');
-
-// const add = require('./utils.js')
-
-// const sum = add(4, -2)
-
-// console.log(sum)
+if(command === 'add'){
+    console.log('Adding note!')
+}else if (command === 'remove'){
+    console.log('Removing note!')
+}
