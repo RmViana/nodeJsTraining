b934c7fa768c70b4417c2173b1aee608d92aaf4f
Romildo Alves <raj.alves96@gmail.com>
2019-07-14 09:42:52 -0300

    Refactoring to use arrow functions

--- starting-over/notes-app/notes.js
+++ starting-over/notes-app/notes.js
@@ -1,15 +1,11 @@
 const fs    = require('fs')
 const chalk = require('chalk')
 
-const getNotes = function(){
-    return 'Your notes...'
-}
+const getNotes = () => 'Your notes...'
 
-const addNote = function(title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter( function(note){
-        return note.title === title
-    })
+    const duplicateNotes = notes.filter((note) => note.title === title)
 
     if(duplicateNotes.length === 0){
         notes.push({
@@ -24,27 +20,23 @@ const addNote = function(title, body) {
     }
 }
 
-const removeNote = function(title){
+const removeNote = (title) => {
     const notes = loadNotes()
-    const newNotes = notes.filter( function(note){
-        return note.title !== title
-    })
+    const newNotes = notes.filter((note) => note.title !== title)
     if(newNotes.length !== notes.length){
         saveNotes(newNotes)
         console.log(chalk.green.inverse(`Note ${title} removed! `))
     }else{
         console.log(chalk.red.inverse('No note found!'))
     }
-
-
 }
 
-const saveNotes = function(notes){
+const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function(){
+const loadNotes = () => {
     try {
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON   = dataBuffer.toString()
