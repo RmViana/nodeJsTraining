1b013158bc4a40d84e49b14d31d0d01304b58b02
Romildo Alves <raj.alves96@gmail.com>
2019-07-14 16:26:19 -0300

    HTTP request challenge

--- starting-over/weather-app/app.js
+++ starting-over/weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const chalk   = require('chalk')
 
 const url = 'https://api.darksky.net/forecast/137746b7b19794ec20897249e41d05ae/37.8267,-122.4233'
 
@@ -7,4 +8,15 @@ request({
     json : true
 }, (error, response) => {
     console.log(`${response.body.daily.data[0].summary} It's currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`)
+})
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoicm12aWFuYSIsImEiOiJjankzNDR3ZXUwb2x6M2xsZ2hnd2ZoN2puIn0.pbhnWi2pWPleL5Mag0bOQQ&limit=1' 
+
+request({
+    url  : geocodeURL,
+    json : true
+}, (error, response) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(chalk.blue.underline(latitude), chalk.blue.underline(longitude))
 })
