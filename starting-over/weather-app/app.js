a999f6059676b33f5c97b1e2d458fc4aca5f3a9a
Romildo Alves <raj.alves96@gmail.com>
2019-07-14 12:23:54 -0300

    Customizing HTTP Requests

--- starting-over/weather-app/app.js
+++ starting-over/weather-app/app.js
@@ -3,8 +3,8 @@ const request = require('request')
 const url = 'https://api.darksky.net/forecast/137746b7b19794ec20897249e41d05ae/37.8267,-122.4233'
 
 request({
-    url : url
+    url : url,
+    json : true
 }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+    console.log(`${response.body.daily.data[0].summary} It's currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`)
 })
