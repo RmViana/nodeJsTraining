c620a1c234fd1f2ee5996d31a955643fc839a8b0
Romildo Alves <raj.alves96@gmail.com>
2019-07-18 21:07:00 -0300

    Destructuring and Property Shorthand Challenge

--- starting-over/weather-app/utils/forecast.js
+++ starting-over/weather-app/utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/137746b7b19794ec20897249e41d05ae/'+ latitude +','+ longitude
     request({
-        url  : url,
+        url,
         json : true
-    }, (error, response) => {
+    }, (error, {body}) => {
         if(error){
             callback('Unable to connect to weather service!')
-        }else if(response.body.error){
-            callback(response.body.error)
+        }else if(body.error){
+            callback(body.error)
         }else{
-            callback(undefined, `${response.body.daily.data[0].summary} It's currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`)
+            callback(undefined, `${body.daily.data[0].summary} It's currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
         }
     })
 }
