635ffdd114dacbe4d490ac0c365d5194c1283f5f
Romildo Alves <raj.alves96@gmail.com>
2019-07-20 11:24:07 -0300

    Serving up HTML and JSON

--- starting-over/web-server/src/app.js
+++ starting-over/web-server/src/app.js
@@ -3,19 +3,30 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Hello express!</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send(
+        [{
+            name : 'Wade Wilson',
+            age : 22
+        },
+        {
+            name : 'Peter Parker',
+            age : 21
+        }])
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h1>About page</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page')
+    res.send({
+        forecast : 'It\'s 20 degrees out there.',
+        location : 'São Paulo'
+    })
 })
 
 app.listen(3000, () => {
