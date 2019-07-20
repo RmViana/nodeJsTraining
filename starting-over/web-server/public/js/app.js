cca6c01b32503c5e75fe71972bc2d313d503ffa4
Romildo Alves <raj.alves96@gmail.com>
2019-07-20 18:16:05 -0300

    Creating a Search Form

--- starting-over/web-server/public/js/app.js
+++ starting-over/web-server/public/js/app.js
@@ -1,18 +1,22 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
 
-fetch('/weather?address=Boston').then((response) =>{
-    response.json().then((data) => {
-        if(!data.error){
-            console.log(data.forecast)
-            console.log(data.location)
-        }else{
-            console.log(data.error)
-        }
+const weatherForm = document.querySelector('form')
+const search      = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const location = search.value
+    const url      = '/weather?address=' + location
+    fetch(url).then((response) =>{
+        response.json().then((data) => {
+            if(!data.error){
+                console.log(data.forecast)
+                console.log(data.location)
+            }else{
+                console.log(data.error)
+            }
+        })
     })
 })
