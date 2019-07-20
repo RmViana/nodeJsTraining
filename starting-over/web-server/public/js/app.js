6125bdbcc17b78520c95f17d9ca2994f8c97c6da
Romildo Alves <raj.alves96@gmail.com>
2019-07-20 17:43:35 -0300

    Browser HTTP Requests with Fetch

--- starting-over/web-server/public/js/app.js
+++ starting-over/web-server/public/js/app.js
@@ -1,1 +1,18 @@
 console.log('Client side javascript file is loaded!')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+fetch('/weather?address=Boston').then((response) =>{
+    response.json().then((data) => {
+        if(!data.error){
+            console.log(data.forecast)
+            console.log(data.location)
+        }else{
+            console.log(data.error)
+        }
+    })
+})
