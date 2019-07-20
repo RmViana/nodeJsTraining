ba81da978fe1df9bc31b2b338dac5853ee7868ce
Romildo Alves <raj.alves96@gmail.com>
2019-07-20 14:57:33 -0300

    Advanced Templating

--- starting-over/web-server/src/app.js
+++ starting-over/web-server/src/app.js
@@ -1,16 +1,18 @@
 const path    = require('path')
 const express = require('express')
+const hbs     = require('hbs') 
 
 const app = express()
 
 //Defines paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath           = path.join(__dirname, '../templates')
-
+const viewsPath           = path.join(__dirname, '../templates/views')
+const partialsPath        = path.join(__dirname, '../templates/partials')
 
 //Setup handlebars engine and view location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -28,7 +30,9 @@ app.get('/about', (req, res) => res.render('about', {
 }))
 
 app.get('/help', (req, res) => res.render('help', {
-    helpText : 'This is some helpful text.'
+    helpText : 'This is some helpful text.',
+    title : 'Help',
+    name : 'Wade Wilson'
 }))
 
 app.get('/weather', (req, res) => {
