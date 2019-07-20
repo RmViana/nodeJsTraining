acd7aafbae642b6c44d1b3c3e0d361f41ec03aa3
Romildo Alves <raj.alves96@gmail.com>
2019-07-20 11:46:40 -0300

    Serving static assets

--- starting-over/web-server/src/app.js
+++ starting-over/web-server/src/app.js
@@ -1,25 +1,13 @@
+const path    = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.send('<h1>Hello express!</h1>')
-})
-
-app.get('/help', (req, res) => {
-    res.send(
-        [{
-            name : 'Wade Wilson',
-            age : 22
-        },
-        {
-            name : 'Peter Parker',
-            age : 21
-        }])
-})
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About page</h1>')
 })
 
 app.get('/weather', (req, res) => {
