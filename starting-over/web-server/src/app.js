3e98f25ca1765f88e8cff84ad52bc94afc255e39
Romildo Alves <raj.alves96@gmail.com>
2019-07-20 14:19:15 -0300

    Dynamic Pages with Templating - Hbs

--- starting-over/web-server/src/app.js
+++ starting-over/web-server/src/app.js
@@ -4,11 +4,24 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.send('<h1>Hello express!</h1>')
+    res.render('index', {
+        title : 'Weather App',
+        name : 'Wade Wilson'
+    })
 })
+
+app.get('/about', (req, res) => res.render('about', {
+    title : 'About me',
+    name : 'Wade Wilson'
+}))
+
+app.get('/help', (req, res) => res.render('help', {
+    helpText : 'This is some helpful text.'
+}))
 
 app.get('/weather', (req, res) => {
     res.send({
