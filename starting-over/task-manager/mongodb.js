8f875bd8a4182442d27335d3db28fd59b5d22b53
Romildo Alves <raj.alves96@gmail.com>
2019-07-27 23:43:46 -0300

    Querying Documents

--- starting-over/task-manager/mongodb.js
+++ starting-over/task-manager/mongodb.js
@@ -10,9 +10,6 @@ const {MongoClient, ObjectID} = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName  = 'task-manager'
 
-const id = new ObjectID()
-console.log(id.id.length)
-console.log(id.toHexString().length)
 // console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, {useNewUrlParser : true}, (error, client) => {
@@ -22,54 +19,38 @@ MongoClient.connect(connectionURL, {useNewUrlParser : true}, (error, client) => 
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     _id  : id,
-    //     name : 'Domino',
-    //     age  : 27
-    // }, (error, result) => {
+    // db.collection('users').findOne({_id : ObjectID('5d3a3923e31e93195086bc99')}, (error, result) => {
     //     if(error){
-    //         return console.log('Unable to insert user!')
+    //         return console.log('Unable to fetch!')
     //     }
-    //     console.log(result.ops)
 
+    //     console.log(result)
     // })
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name : 'Jessie',
-    //         age : 21
-    //     },
-    //     {
-    //         name : 'Júlio',
-    //         age : 22
-    //     }
-    // ], (error, result) => {
-    //     if(error){
-    //         return console.log('Unable to insert documents!')
-    //     }
-
-    //     console.log(result.ops)
+    // db.collection('users').find({age : 22}).toArray((error, users) => {
+    //     console.log(users)
     // })
 
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description : 'Finish Agatha Chritie\'s book',
-    //         completed : false
-    //     },
-    //     {
-    //         description : 'Learn a bit about Git and Github',
-    //         completed : true
-    //     },
-    //     {
-    //         description : 'To watch Endgame',
-    //         completed : true
-    //     }
-    // ], (error, result) => {
-    //     if(error){
-    //         return console.log('Unable to insert documents!')
-    //     }
-    //     console.log(result.ops)
+    // db.collection('users').find({age : 22}).count((error, count) => {
+    //     console.log(count)
     // })
+
+    db.collection('tasks').findOne({_id : new ObjectID('5d3a3a593ae17023b03f2247')}, (error, result) => {
+        if(error){
+            return console.log('Unable to fetch data!')
+        }
+
+        console.log(result)
+    })
+
+    db.collection('tasks').find({completed : false}).toArray((error, result) => {
+        if(error){
+            return console.log('Unable to fetch data!')
+        }
+
+        console.log(result)
+    })
+
 
 })
 
