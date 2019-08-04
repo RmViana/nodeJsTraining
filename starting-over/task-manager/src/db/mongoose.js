3b12c7675ae1e6f60ed8fc4e06eb9531ba1199a7
Romildo Alves <raj.alves96@gmail.com>
2019-08-04 18:18:58 -0300

    Resource Creation Endpoints: Part II

--- starting-over/task-manager/src/db/mongoose.js
+++ starting-over/task-manager/src/db/mongoose.js
@@ -4,26 +4,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser : true,
     useCreateIndex : true
 })
-
-const Task = mongoose.model('Task', {
-    description : {
-        type : String,
-        required : true,
-        trim : true
-    },
-    completed : {
-        type : Boolean,
-        required : false,
-        default : false
-    }
-})
-
-// const task = new Task({
-//     description : '       Watch MasterChef'
-// })
-
-// task.save().then(() => {
-//     console.log(task)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
