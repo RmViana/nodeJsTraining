0918c4b6b374ee174897309960b73fd055d53410
Romildo Alves <raj.alves96@gmail.com>
2019-08-10 20:34:25 -0300

    Async/ Await: Part II

--- starting-over/task-manager/playground/promise-chaining-2.js
+++ starting-over/task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5d4749e3f3c0b83e98c7f604').then((task) => {
-    console.log(task)
-    return Task.countDocuments({completed : false})
-}).then((result) => {
-    console.log(result)
+// Task.findByIdAndDelete('5d4749e3f3c0b83e98c7f604').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({completed : false})
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id, completed) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({completed})
+    return count
+}
+
+deleteTaskAndCount('5d47491e4ebc5b177c5e4390', false).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
