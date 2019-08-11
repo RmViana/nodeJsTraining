d40f988e799545ad89f3a38fd9b7aa858e62314d
Romildo Alves <raj.alves96@gmail.com>
2019-08-11 19:18:14 -0300

    Integrating Async/Await

--- starting-over/task-manager/src/index.js
+++ starting-over/task-manager/src/index.js
@@ -10,71 +10,76 @@ app.use(express.json())
 
 
 //Create User
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
-    user.save().then(() => {
+    try{
+        await user.save()    
         res.status(201).send(user)
-    }).catch((e) => {
+    }catch(e){
         res.status(400).send(e)
-    })
+    }
 })
 
 //Read all Users
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({}) 
         res.send(users)
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send()
-    })
+    }
 })
 
 
 //Read specific User
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async(req, res) => {
     const _id = req.params.id
-    User.findById(_id).then((user) => {
+    try{
+        const user = await User.findById(_id)
         if(!user){
             return res.status(404).send()
         }
         res.send(user)
-    }).catch((e) => {
+    }catch(e){
         res.status(500).send()
-    })
-
+    }
 })
 
 
 //Create Task
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
-    task.save().then(() => {
+    try{
+        task.save()
         res.status(201).send(task)
-    }).catch((e) => {
+    }catch(e){
         res.status(400).send(e)
-    })
+    }
 })
 
 //Read All Tasks
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+app.get('/tasks', async (req, res) => {
+    try{
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch((e) => {
+    }catch(e){
         res.status(500).send()
-    })
+    }
 })
 
 
 //Read specific Task
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
-    Task.findById(_id).then((task) => {
+    try{
+        const task = await Task.findById(_id)
         if(!task){
             return res.status(404).send()
         }
         res.send(task)
-    }).catch((e) => {
+    }catch(e){
         res.status(500).send()
-    })
+    }
 })
 
 //Start the server on the chosen port
