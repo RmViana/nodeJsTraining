fe9674de37731155cb73f9048ac100fe6b3b65ca
Romildo Alves <raj.alves96@gmail.com>
2019-08-13 21:32:43 -0300

    Resource Updating Endpoints: Part I

--- starting-over/task-manager/src/index.js
+++ starting-over/task-manager/src/index.js
@@ -8,10 +8,6 @@ const port    = process.env.PORT || 3000
 
 app.use(express.json())
 
-//Implemented: Async/ Await
-
-
-//Create User
 app.post('/users', async (req, res) => {
     const user = new User(req.body)
     try{
@@ -22,7 +18,6 @@ app.post('/users', async (req, res) => {
     }
 })
 
-//Read all Users
 app.get('/users', async (req, res) => {
     try {
         const users = await User.find({}) 
@@ -33,7 +28,6 @@ app.get('/users', async (req, res) => {
 })
 
 
-//Read specific User
 app.get('/users/:id', async(req, res) => {
     const _id = req.params.id
     try{
@@ -47,8 +41,29 @@ app.get('/users/:id', async(req, res) => {
     }
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-//Create Task
+    if(!isValidOperation){
+        return res.status(400).send({error : 'Invalid updates!'})
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new : true, runValidators : true})
+
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
     try{
@@ -59,7 +74,6 @@ app.post('/tasks', async (req, res) => {
     }
 })
 
-//Read All Tasks
 app.get('/tasks', async (req, res) => {
     try{
         const tasks = await Task.find({})
@@ -69,8 +83,6 @@ app.get('/tasks', async (req, res) => {
     }
 })
 
-
-//Read specific Task
 app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
     try{
@@ -84,7 +96,8 @@ app.get('/tasks/:id', async (req, res) => {
     }
 })
 
-//Start the server on the chosen port
+
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
