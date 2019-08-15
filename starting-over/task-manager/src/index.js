899c0cf372a00a10646912bd23abb5c6da8db2a6
Romildo Alves <raj.alves96@gmail.com>
2019-08-15 20:38:47 -0300

    Resource Deleting Endpoints

--- starting-over/task-manager/src/index.js
+++ starting-over/task-manager/src/index.js
@@ -64,6 +64,20 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
     try{
@@ -113,6 +127,20 @@ app.patch('/tasks/:id', async (req, res) => {
         res.send(task)
     }catch(e){
         res.status(400).send()
+    }
+})
+
+app.delete('/tasks/:id', async(req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if(!task){
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
     }
 })
 
