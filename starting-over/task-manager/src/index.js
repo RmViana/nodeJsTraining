25618443bb452d361ad74e8f0a5e640010ec135a
Romildo Alves <raj.alves96@gmail.com>
2019-08-20 21:29:26 -0300

    Accepting Authentication Tokens

--- starting-over/task-manager/src/index.js
+++ starting-over/task-manager/src/index.js
@@ -3,6 +3,7 @@ require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
+
 const app     = express()
 const port    = process.env.PORT || 3000
 
@@ -14,9 +15,9 @@ const port    = process.env.PORT || 3000
 //     }
 // })
 
-app.use((req, res, next) => {
-    res.status(503).send('Site is currently down. Check back soon!')
-})
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently down. Check back soon!')
+// })
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
