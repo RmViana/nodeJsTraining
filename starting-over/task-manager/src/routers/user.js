57f4ca57b8adb7945e3ac5e570597dd2d3b35fd9
Romildo Alves <raj.alves96@gmail.com>
2019-08-25 17:16:41 -0300

    Authenticating User Endpoints

--- starting-over/task-manager/src/routers/user.js
+++ starting-over/task-manager/src/routers/user.js
@@ -51,20 +51,7 @@ router.get('/users/me', auth, async (req, res) => {
 })
 
 
-router.get('/users/:id', async(req, res) => {
-    const _id = req.params.id
-    try{
-        const user = await User.findById(_id)
-        if(!user){
-            return res.status(404).send()
-        }
-        res.send(user)
-    }catch(e){
-        res.status(500).send()
-    }
-})
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -75,7 +62,7 @@ router.patch('/users/:id', async (req, res) => {
 
     try {
 
-        const user = await User.findById(req.params.id)
+        const user = req.user
         updates.forEach((update) => user[update] = req.body[update])
 
         await user.save()
@@ -91,15 +78,11 @@ router.patch('/users/:id', async (req, res) => {
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
+        await req.user.remove()
 
-        if(!user){
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        res.send(req.user)
     } catch (e) {
         res.status(500).send(e)
     }
