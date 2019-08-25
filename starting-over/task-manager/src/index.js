365ed9a6cc5b41a48ea15e80231bdd69d8c0f1ae
Romildo Alves <raj.alves96@gmail.com>
2019-08-25 18:03:47 -0300

    The User/Task Relationship

--- starting-over/task-manager/src/index.js
+++ starting-over/task-manager/src/index.js
@@ -25,3 +25,18 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const User = require('./models/user')
+const Task = require('./models/task')
+
+const main = async () => {
+    // const task = await Task.findById('5d62f3d8f26d9911a0950acb')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    const user = await User.findById('5d62f1f41e871120b0b1f66c')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
+}
+
+main()
