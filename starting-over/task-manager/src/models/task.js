eddc07e575ab367746eb7cc3ccf196566c9eafc1
Romildo Alves <raj.alves96@gmail.com>
2019-08-29 23:25:27 -0300

    Filtering Data

--- starting-over/task-manager/src/models/task.js
+++ starting-over/task-manager/src/models/task.js
@@ -1,38 +1,24 @@
 const mongoose = require('mongoose')
 
-const taskSchema = mongoose.Schema({
-    description : {
-        type : String,
-        required : true,
-        trim : true
+const taskSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: true,
+        trim: true
     },
-    completed : {
-        type : Boolean,
-        required : false,
-        default : false
+    completed: {
+        type: Boolean,
+        default: false
     },
-    owner : {
-        type : mongoose.Schema.Types.ObjectId,
-        required : true,
-        ref : 'User'
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
     }
 }, {
-    timestamps : true
+    timestamps: true
 })
-
-taskSchema.pre('save', async function(next) {
-    const task = this
-
-    if(task.isModified('completed')){
-        console.log('Task status changed!')
-    }
-
-    next()
-})
-
 
 const Task = mongoose.model('Task', taskSchema)
-
-
 
 module.exports = Task
