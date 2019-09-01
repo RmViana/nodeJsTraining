5786b9eb4f594f28c91d5b7574f4c7ea40798f9a
Romildo Alves <raj.alves96@gmail.com>
2019-09-01 13:13:17 -0300

    Testing Asynchronous Code

--- starting-over/task-manager/tests/math.test.js
+++ starting-over/task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math')
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
@@ -18,6 +18,25 @@ test('Should convert 32 F to 0 C', () => {
 test('Should convert 0 C to 32 F', () => {
     const temp = celsiusToFahrenheit(0)
     expect(temp).toBe(32)
+})
+
+// test('Asyn test demo', (done) =>{
+//     setTimeout(() => {
+//         expect(1).toBe(2)
+//         done()
+//     }, 2000);
+// })
+
+test('Should add two numbers', (done) => {
+    add(2, 3).then((sum) => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('Should add two number async/await', async() => {
+    const sum = await add(11, 22)
+    expect(sum).toBe(32)
 })
 
 // 
