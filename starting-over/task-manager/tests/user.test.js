5c87ebcd8b0672a1dde5685147ea0969f7233a47
Romildo Alves <raj.alves96@gmail.com>
2019-09-01 19:54:32 -0300

    Advaced Assertions

--- starting-over/task-manager/tests/user.test.js
+++ starting-over/task-manager/tests/user.test.js
@@ -22,18 +22,37 @@ beforeEach(async () => {
 })
 
 test('Should signup a new user', async() => {
-    await request(app).post('/users').send({
+    const response = await request(app).post('/users').send({
         name : 'Wade Wilson',
         email : 'raj.alves96@gmail.com',
         password : 'MyPass@777!'
     }).expect(201)
+
+    //Assert that the database was changed correctly
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+
+    //Assertions about the response
+    expect(response.body).toMatchObject({
+        user : {
+            name : 'Wade Wilson',
+            email : 'raj.alves96@gmail.com'
+        },
+        token : user.tokens[0].token
+    })
+
+    expect(user.password).not.toBe('MyPass@777!')
 })
 
 test('Should login existing user', async() => {
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email : userOne.email,
         password : userOne.password
     }).expect(200)
+
+    const user = await User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+    expect(response.body.token).toBe(user.tokens[1].token)
 })
 
 test('Should not login nonexisting user', async() => {
@@ -59,11 +78,13 @@ test('Should not get profile for unauthenticated user', async() => {
 })
 
 test('Should delete account for user', async() => {
-    await request(app)
+    const response = await request(app)
             .delete('/users/me')
             .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
             .send()
             .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
 })
 
 test('Should not delete account for unauthenticated user', async() => {
